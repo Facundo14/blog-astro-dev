dc2ee15c2ac1218622741e36148e2cc3762c92ff
Tanovich Facundo <facundotanovichdev@gmail.com>
2024-06-26 19:07:30 -0300

    Primer commit

---
+++ src/config/site-config.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+    title: 'Blog de Facundo Tanovich',
+    description: 'Blog de Facundo Tanovich, desarrollador web full stack. Aquí encontrarás artículos sobre desarrollo web, programación, tecnología y más.',
+}
---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,27 @@
+import rss from '@astrojs/rss';
+import type { APIRoute } from 'astro';
+import { getCollection } from 'astro:content';
+
+export const GET: APIRoute = async ({ params, request, site }) => {
+    const blogPost = await getCollection('blog');
+    return rss({
+        // stylesheet: '/styles/rss.xsl',
+        // `<title>` field in output xml
+        title: 'Blog de Facundo',
+        // `<description>` field in output xml
+        description: 'Blog personal de Facundo Corradini',
+        // Pull in your project "site" from the endpoint context
+        // https://docs.astro.build/en/reference/api-reference/#contextsite
+        site: site ?? '',
+        // Array of `<item>`s in output xml
+        // See "Generating items" section for examples using content collections and glob imports
+        items: blogPost.map(({ data, slug }) => ({
+            title: data.title,
+            pubDate: data.date,
+            link: `/posts/${slug}`,
+            description: data.description,
+        })),
+        // (optional) inject custom xml
+        customData: `<language>es-ES</language>`,
+    });
+}
